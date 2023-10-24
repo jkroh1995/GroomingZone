c97dd5c0048d760063356fd4faf9fb60432dfbf9
jkroh1995 <shworud29@gmail.com>
2023-10-24 16:39:24 +0900

    init : first commit

---
+++ src/main/java/tdd/groomingzone/GroomingzoneApplication.java
@@ -0,0 +1,13 @@
+package tdd.groomingzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomingzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroomingzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tdd/groomingzone/GroomingzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package tdd.groomingzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomingzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
