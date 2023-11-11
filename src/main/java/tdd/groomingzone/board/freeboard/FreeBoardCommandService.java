e9a11acd97d1088e19d60a07077ab432cc6808d1
jkroh1995 <shworud29@gmail.com>
2023-11-11 22:31:55 +0900

    feat : 자유 게시글 삭제 기능 구현

--- src/main/java/tdd/groomingzone/board/freeboard/FreeBoardCommandService.java
+++ src/main/java/tdd/groomingzone/board/freeboard/FreeBoardCommandService.java
@@ -1,6 +1,7 @@
 package tdd.groomingzone.board.freeboard;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class FreeBoardCommandService {
@@ -11,11 +12,18 @@ public class FreeBoardCommandService {
         this.freeBoardRepository = freeBoardRepository;
     }
 
+    @Transactional
     public FreeBoard create(FreeBoard entity) {
         return freeBoardRepository.save(entity);
     }
 
+    @Transactional
     public void update(FreeBoard entity, FreeBoardDto.Put putDto) {
         entity.modify(putDto);
+    }
+
+    @Transactional
+    public void delete(FreeBoard entity) {
+        freeBoardRepository.delete(entity);
     }
 }
