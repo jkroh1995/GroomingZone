11646048ff7824d22138ce7f63ee487483987889
jkroh1995 <shworud29@gmail.com>
2023-11-27 16:09:08 +0900

    feat : Controller 자유 게시글 수정 기능 구현

--- src/main/java/tdd/groomingzone/board/freeboard/FreeBoardController.java
+++ src/main/java/tdd/groomingzone/board/freeboard/FreeBoardController.java
@@ -2,10 +2,7 @@ package tdd.groomingzone.board.freeboard;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/free-board")
@@ -21,5 +18,12 @@ public class FreeBoardController {
     public ResponseEntity<FreeBoardDto.Response> postFreeBoard(@RequestBody FreeBoardDto.Post dto) {
         FreeBoardDto.Response responseDto = freeBoardService.postFreeBoard(dto);
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{free-board-id}")
+    public ResponseEntity<FreeBoardDto.Response> putFreeBoard(@PathVariable("free-board-id") long freeBoardId,
+                                                              @RequestBody FreeBoardDto.Put dto){
+        FreeBoardDto.Response responseDto = freeBoardService.putFreeBoard(freeBoardId, dto);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 }
