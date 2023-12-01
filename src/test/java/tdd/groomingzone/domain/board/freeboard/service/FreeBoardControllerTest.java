a7b1712f3045007f006aab5822a15ae460f28dc7
jkroh1995 <shworud29@gmail.com>
2023-12-01 17:28:00 +0900

    test : 페이징 처리 완료를 위한 테스트 작성

--- src/test/java/tdd/groomingzone/domain/board/freeboard/service/FreeBoardControllerTest.java
+++ src/test/java/tdd/groomingzone/domain/board/freeboard/service/FreeBoardControllerTest.java
@@ -7,11 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import tdd.groomingzone.domain.board.freeboard.dto.FreeBoardDto;
 import tdd.groomingzone.domain.board.freeboard.controller.FreeBoardController;
+import tdd.groomingzone.domain.board.freeboard.entity.FreeBoard;
+import tdd.groomingzone.global.pagedresponse.PagedResponseDto;
 import tdd.groomingzone.util.StubTime;
 
 import java.time.LocalDateTime;
@@ -214,7 +218,17 @@ class FreeBoardControllerTest {
     @DisplayName("게시글 페이지 조회 요청 테스트")
     void getFreeBoardPageTest() throws Exception {
         // given
-        int fakePage = 1;
+        int fakePageNumber = 1;
+
+        Page<FreeBoard> fakePage = new PageImpl<>(List.of(
+                FreeBoard.builder().title("1").content("1").build(),
+                FreeBoard.builder().title("2").content("2").build(),
+                FreeBoard.builder().title("3").content("3").build(),
+                FreeBoard.builder().title("4").content("4").build(),
+                FreeBoard.builder().title("5").content("5").build(),
+                FreeBoard.builder().title("6").content("6").build(),
+                FreeBoard.builder().title("6").content("6").build()
+        ));
 
         List<FreeBoardDto.Response> fakeResponseList = List.of(
                 FreeBoardDto.Response.builder().boardId(1).title("1").content("1").viewCount(1).createdAt("1").modifiedAt("1").build(),
@@ -225,25 +239,25 @@ class FreeBoardControllerTest {
                 FreeBoardDto.Response.builder().boardId(5).title("6").content("6").viewCount(1).createdAt("6").modifiedAt("6").build()
         );
 
-        given(freeBoardService.getFreeBoardPage(anyInt())).willReturn(fakeResponseList);
+        given(freeBoardService.getFreeBoardPage(anyInt())).willReturn(new PagedResponseDto<>(fakeResponseList, fakePage));
 
         // when // then
         for (int i = 0; i < fakeResponseList.size(); i++) {
             mockMvc.perform(
-                            get("/free-board?page={page}", fakePage)
+                            get("/free-board?page={page}", fakePageNumber)
                                     .accept(MediaType.APPLICATION_JSON)
                                     .contentType(MediaType.APPLICATION_JSON)
-                    ).andExpect(jsonPath("$", hasSize(fakeResponseList.size())))
-                    .andExpect(jsonPath("$[%d].boardId", i).value(fakeResponseList.get(i).boardId))
-                    .andExpect(jsonPath("$[%d].title", i).value(fakeResponseList.get(i).title))
-                    .andExpect(jsonPath("$[%d].content", i).value(fakeResponseList.get(i).content))
-                    .andExpect(jsonPath("$[%d].viewCount", i).value(fakeResponseList.get(i).viewCount))
-                    .andExpect(jsonPath("$[%d].createdAt", i).value(fakeResponseList.get(i).createdAt))
-                    .andExpect(jsonPath("$[%d].modifiedAt", i).value(fakeResponseList.get(i).modifiedAt));
+                    ).andExpect(jsonPath("data", hasSize(fakeResponseList.size())))
+                    .andExpect(jsonPath("data[%d].boardId", i).value(fakeResponseList.get(i).boardId))
+                    .andExpect(jsonPath("data[%d].title", i).value(fakeResponseList.get(i).title))
+                    .andExpect(jsonPath("data[%d].content", i).value(fakeResponseList.get(i).content))
+                    .andExpect(jsonPath("data[%d].viewCount", i).value(fakeResponseList.get(i).viewCount))
+                    .andExpect(jsonPath("data[%d].createdAt", i).value(fakeResponseList.get(i).createdAt))
+                    .andExpect(jsonPath("data[%d].modifiedAt", i).value(fakeResponseList.get(i).modifiedAt));
         }
 
         mockMvc.perform(
-                        get("/free-board?page={page}", fakePage)
+                        get("/free-board?page={page}", fakePageNumber)
                                 .accept(MediaType.APPLICATION_JSON)
                                 .contentType(MediaType.APPLICATION_JSON)
                 ).andExpect(status().isOk())
@@ -256,12 +270,16 @@ class FreeBoardControllerTest {
                         ),
                         responseFields(
                                 List.of(
-                                        fieldWithPath("[].boardId").type(JsonFieldType.NUMBER).description("게시글 식별자"),
-                                        fieldWithPath("[].title").type(JsonFieldType.STRING).description("제목"),
-                                        fieldWithPath("[].content").type(JsonFieldType.STRING).description("내용"),
-                                        fieldWithPath("[].viewCount").type(JsonFieldType.NUMBER).description("조회수"),
-                                        fieldWithPath("[].createdAt").type(JsonFieldType.STRING).description("작성일"),
-                                        fieldWithPath("[].modifiedAt").type(JsonFieldType.STRING).description("수정일")
+                                        fieldWithPath("data[].boardId").type(JsonFieldType.NUMBER).description("게시글 식별자"),
+                                        fieldWithPath("data[].title").type(JsonFieldType.STRING).description("제목"),
+                                        fieldWithPath("data[].content").type(JsonFieldType.STRING).description("내용"),
+                                        fieldWithPath("data[].viewCount").type(JsonFieldType.NUMBER).description("조회수"),
+                                        fieldWithPath("data[].createdAt").type(JsonFieldType.STRING).description("작성일"),
+                                        fieldWithPath("data[].modifiedAt").type(JsonFieldType.STRING).description("수정일"),
+                                        fieldWithPath("pageInfo.page").type(JsonFieldType.NUMBER).description("페이지 번호"),
+                                        fieldWithPath("pageInfo.size").type(JsonFieldType.NUMBER).description("해당 페이지에 담긴 게시글 수"),
+                                        fieldWithPath("pageInfo.totalElements").type(JsonFieldType.NUMBER).description("전체 게시글 수"),
+                                        fieldWithPath("pageInfo.totalPage").type(JsonFieldType.NUMBER).description("전체 페이지 수")
                                 )
                         )
                 ));
@@ -271,10 +289,18 @@ class FreeBoardControllerTest {
     @DisplayName("게시글 검색 요청 테스트")
     void getFilteredFreeBoardPageTest() throws Exception {
         // given
-        int fakePage = 1;
+        int fakePageNumber = 1;
         String fakeTitle = "title";
         String fakeContent = "content";
         String fakeWriter = "writer";
+
+        Page<FreeBoard> fakePage = new PageImpl<>(List.of(
+                FreeBoard.builder().title("1").content("1").build(),
+                FreeBoard.builder().title("2").content("2").build(),
+                FreeBoard.builder().title("3").content("3").build(),
+                FreeBoard.builder().title("4").content("4").build(),
+                FreeBoard.builder().title("5").content("5").build()
+        ));
 
         List<FreeBoardDto.Response> fakeResponseList = List.of(
                 FreeBoardDto.Response.builder().boardId(1).title("1").content("1").viewCount(1).createdAt("1").modifiedAt("1").build(),
@@ -285,25 +311,25 @@ class FreeBoardControllerTest {
                 FreeBoardDto.Response.builder().boardId(5).title("6").content("6").viewCount(1).createdAt("6").modifiedAt("6").build()
         );
 
-        given(freeBoardService.getFilteredFreeBoardList(anyString(), anyString(), anyString(), anyInt())).willReturn(fakeResponseList);
+        given(freeBoardService.getFilteredFreeBoardList(anyString(), anyString(), anyString(), anyInt())).willReturn(new PagedResponseDto<>(fakeResponseList, fakePage));
 
         // when // then
         for (int i = 0; i < fakeResponseList.size(); i++) {
             mockMvc.perform(
-                            get("/free-board/search?title={title}&content={content}&writer={writer}&page={page}", fakeTitle, fakeContent, fakeWriter, fakePage)
+                            get("/free-board/search?title={title}&content={content}&writer={writer}&page={page}", fakeTitle, fakeContent, fakeWriter, fakePageNumber)
                                     .accept(MediaType.APPLICATION_JSON)
                                     .contentType(MediaType.APPLICATION_JSON)
-                    ).andExpect(jsonPath("$", hasSize(fakeResponseList.size())))
-                    .andExpect(jsonPath("$[%d].boardId", i).value(fakeResponseList.get(i).boardId))
-                    .andExpect(jsonPath("$[%d].title", i).value(fakeResponseList.get(i).title))
-                    .andExpect(jsonPath("$[%d].content", i).value(fakeResponseList.get(i).content))
-                    .andExpect(jsonPath("$[%d].viewCount", i).value(fakeResponseList.get(i).viewCount))
-                    .andExpect(jsonPath("$[%d].createdAt", i).value(fakeResponseList.get(i).createdAt))
-                    .andExpect(jsonPath("$[%d].modifiedAt", i).value(fakeResponseList.get(i).modifiedAt));
+                    ).andExpect(jsonPath("data", hasSize(fakeResponseList.size())))
+                    .andExpect(jsonPath("data[%d].boardId", i).value(fakeResponseList.get(i).boardId))
+                    .andExpect(jsonPath("data[%d].title", i).value(fakeResponseList.get(i).title))
+                    .andExpect(jsonPath("data[%d].content", i).value(fakeResponseList.get(i).content))
+                    .andExpect(jsonPath("data[%d].viewCount", i).value(fakeResponseList.get(i).viewCount))
+                    .andExpect(jsonPath("data[%d].createdAt", i).value(fakeResponseList.get(i).createdAt))
+                    .andExpect(jsonPath("data[%d].modifiedAt", i).value(fakeResponseList.get(i).modifiedAt));
         }
 
         mockMvc.perform(
-                        get("/free-board/search?title={title}&content={content}&writer={writer}&page={page}", fakeTitle, fakeContent, fakeWriter, fakePage)
+                        get("/free-board/search?title={title}&content={content}&writer={writer}&page={page}", fakeTitle, fakeContent, fakeWriter, fakePageNumber)
                                 .accept(MediaType.APPLICATION_JSON)
                                 .contentType(MediaType.APPLICATION_JSON)
                 ).andExpect(status().isOk())
@@ -319,12 +345,16 @@ class FreeBoardControllerTest {
                         ),
                         responseFields(
                                 List.of(
-                                        fieldWithPath("[].boardId").type(JsonFieldType.NUMBER).description("게시글 식별자"),
-                                        fieldWithPath("[].title").type(JsonFieldType.STRING).description("제목"),
-                                        fieldWithPath("[].content").type(JsonFieldType.STRING).description("내용"),
-                                        fieldWithPath("[].viewCount").type(JsonFieldType.NUMBER).description("조회수"),
-                                        fieldWithPath("[].createdAt").type(JsonFieldType.STRING).description("작성일"),
-                                        fieldWithPath("[].modifiedAt").type(JsonFieldType.STRING).description("수정일")
+                                        fieldWithPath("data[].boardId").type(JsonFieldType.NUMBER).description("게시글 식별자"),
+                                        fieldWithPath("data[].title").type(JsonFieldType.STRING).description("제목"),
+                                        fieldWithPath("data[].content").type(JsonFieldType.STRING).description("내용"),
+                                        fieldWithPath("data[].viewCount").type(JsonFieldType.NUMBER).description("조회수"),
+                                        fieldWithPath("data[].createdAt").type(JsonFieldType.STRING).description("작성일"),
+                                        fieldWithPath("data[].modifiedAt").type(JsonFieldType.STRING).description("수정일"),
+                                        fieldWithPath("pageInfo.page").type(JsonFieldType.NUMBER).description("페이지 번호"),
+                                        fieldWithPath("pageInfo.size").type(JsonFieldType.NUMBER).description("해당 페이지에 담긴 게시글 수"),
+                                        fieldWithPath("pageInfo.totalElements").type(JsonFieldType.NUMBER).description("전체 게시글 수"),
+                                        fieldWithPath("pageInfo.totalPage").type(JsonFieldType.NUMBER).description("전체 페이지 수")
                                 )
                         )
                 ));
