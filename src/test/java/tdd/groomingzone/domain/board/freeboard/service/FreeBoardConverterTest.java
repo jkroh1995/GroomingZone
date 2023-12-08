3c064a5be75419449f2bc4888b449f7189bdf889
jkroh1995 <shworud29@gmail.com>
2023-12-08 17:48:18 +0900

    test : 게시글 작성 시 인증된 사용자 정보 등록 기능 테스트 작성

--- src/test/java/tdd/groomingzone/domain/board/freeboard/service/FreeBoardConverterTest.java
+++ src/test/java/tdd/groomingzone/domain/board/freeboard/service/FreeBoardConverterTest.java
@@ -1,8 +1,10 @@
 package tdd.groomingzone.domain.board.freeboard.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import tdd.groomingzone.domain.board.freeboard.entity.FreeBoard;
 import tdd.groomingzone.domain.board.freeboard.dto.FreeBoardDto;
+import tdd.groomingzone.domain.member.entity.Member;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,15 +28,24 @@ class FreeBoardConverterTest {
     }
 
     @Test
+    @DisplayName("Entity를 Response DTO로 변환한다.")
     void convertEntityToResponseDtoTest(){
+        Member member = Member.builder()
+                .name("name")
+                .build();
+        member.setId(1L);
+
         FreeBoard entity = FreeBoard.builder()
                 .title("title")
                 .content("content")
                 .build();
+        member.writeFreeBoard(entity);
 
         FreeBoardDto.Response responseDto = freeBoardConverter.convertEntityToResponseDto(entity);
 
         assertThat(entity.getTitle()).isEqualTo(responseDto.title);
         assertThat(entity.getContent()).isEqualTo(responseDto.content);
+        assertThat(entity.getWriter().getId()).isEqualTo(responseDto.writerId);
+        assertThat(entity.getWriter().getName()).isEqualTo(responseDto.writerName);
     }
 }
