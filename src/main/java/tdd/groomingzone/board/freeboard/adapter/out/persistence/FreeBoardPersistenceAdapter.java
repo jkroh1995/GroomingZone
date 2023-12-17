9f627f0c9285e7d319da6f2a86c742ce4980c97a
jkroh1995 <shworud29@gmail.com>
2023-12-17 19:07:45 +0900

    refactor : 게시글 수정 기능 리팩토링

--- src/main/java/tdd/groomingzone/board/freeboard/adapter/out/persistence/FreeBoardPersistenceAdapter.java
+++ src/main/java/tdd/groomingzone/board/freeboard/adapter/out/persistence/FreeBoardPersistenceAdapter.java
@@ -2,26 +2,42 @@ package tdd.groomingzone.board.freeboard.adapter.out.persistence;
 
 import org.springframework.stereotype.Repository;
 
-import tdd.groomingzone.board.freeboard.application.port.out.FreeBoardQueryResult;
-import tdd.groomingzone.board.freeboard.application.port.out.SaveFreeBoardPort;
-
-import tdd.groomingzone.board.freeboard.application.port.out.SaveFreeBoardQuery;
+import tdd.groomingzone.board.freeboard.application.port.out.*;
+import tdd.groomingzone.board.freeboard.domain.FreeBoard;
+import tdd.groomingzone.global.exception.BusinessException;
+import tdd.groomingzone.global.exception.ExceptionCode;
+import tdd.groomingzone.member.application.port.LoadMemberPort;
+import tdd.groomingzone.member.domain.Member;
 
 @Repository
-public class FreeBoardPersistenceAdapter implements SaveFreeBoardPort {
-
+public class FreeBoardPersistenceAdapter implements SaveFreeBoardPort, LoadFreeBoardPort {
+    private final LoadMemberPort loadMemberPort;
     private final FreeBoardEntityRepository freeBoardEntityRepository;
     private final FreeBoardMapper freeBoardMapper;
 
-    public FreeBoardPersistenceAdapter(FreeBoardEntityRepository freeBoardEntityRepository, FreeBoardMapper freeBoardMapper) {
+    public FreeBoardPersistenceAdapter(LoadMemberPort loadMemberPort, FreeBoardEntityRepository freeBoardEntityRepository, FreeBoardMapper freeBoardMapper) {
+        this.loadMemberPort = loadMemberPort;
         this.freeBoardEntityRepository = freeBoardEntityRepository;
         this.freeBoardMapper = freeBoardMapper;
     }
 
     @Override
-    public FreeBoardQueryResult save(SaveFreeBoardQuery query) {
-        FreeBoardEntity freeBoardEntity = freeBoardMapper.mapToDatabaseEntity(query);
-        FreeBoardEntity savedFreeBoardEntity =  freeBoardEntityRepository.save(freeBoardEntity);
-        return freeBoardMapper.mapToQueryResult(savedFreeBoardEntity);
+    public FreeBoard save(long writerId, FreeBoard freeBoard) {
+        FreeBoardEntity databaseEntity = freeBoardMapper.mapToDatabaseEntity(writerId, freeBoard);
+        FreeBoardEntity savedFreeBoardEntity =  freeBoardEntityRepository.save(databaseEntity);
+        Member writer = loadMemberPort.findMemberById(writerId);
+        return freeBoardMapper.mapToDomainEntity(writer, savedFreeBoardEntity);
+    }
+
+    @Override
+    public FreeBoard loadFreeBoardById(long freeBoardId) {
+        FreeBoardEntity findFreeBoardEntity = findFreeBoardByIdOrElseThrowException(freeBoardId);
+        Member writer = loadMemberPort.findMemberById(findFreeBoardEntity.getMemberId());
+        return freeBoardMapper.mapToDomainEntity(writer, findFreeBoardEntity);
+    }
+
+    private FreeBoardEntity findFreeBoardByIdOrElseThrowException(long freeBoardId) {
+        return freeBoardEntityRepository.findById(freeBoardId).orElseThrow(() ->
+                new BusinessException(ExceptionCode.BOARD_NOT_FOUND));
     }
 }
