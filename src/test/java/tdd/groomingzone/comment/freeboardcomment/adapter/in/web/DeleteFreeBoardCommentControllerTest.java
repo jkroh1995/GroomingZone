79ac5fa95bbf57842c48ebfdf249c5c9653d1a5a
jkroh1995 <shworud29@gmail.com>
2024-01-01 21:48:04 +0900

    test : 자유 게시글 댓글 삭제 테스트 작성

---
+++ src/test/java/tdd/groomingzone/comment/freeboardcomment/adapter/in/web/DeleteFreeBoardCommentControllerTest.java
@@ -0,0 +1,51 @@
+package tdd.groomingzone.comment.freeboardcomment.adapter.in.web;
+
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import tdd.groomingzone.comment.freeboardcomment.application.port.in.usecase.DeleteFreeBoardCommentUseCase;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static tdd.groomingzone.global.utils.ApiDocumentUtils.getRequestPreProcessor;
+import static tdd.groomingzone.global.utils.ApiDocumentUtils.getResponsePreProcessor;
+
+@WebMvcTest(controllers = DeleteFreeBoardCommentController.class,
+        excludeAutoConfiguration = {SecurityAutoConfiguration.class})
+@AutoConfigureRestDocs
+class DeleteFreeBoardCommentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void deleteFreeBoardComment() throws Exception {
+        // given
+        long testId = 1L;
+
+        // when // then
+        mockMvc.perform(
+                        delete("/free-board/comment/{free-board-id}/{comment-id}", testId, 1L)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                ).andExpect(status().isNoContent())
+                .andDo(document(
+                        "delete-free-board-comment",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        pathParameters(
+                                parameterWithName("free-board-id").description("자유 게시글 식별자"),
+                                parameterWithName("comment-id").description("자유 게시글 댓글 식별자")
+                        )
+                ));
+    }
+}
---
+++ src/test/java/tdd/groomingzone/comment/freeboardcomment/application/service/DeleteFreeBoardCommentServiceTest.java
@@ -0,0 +1,105 @@
+package tdd.groomingzone.comment.freeboardcomment.application.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tdd.groomingzone.board.freeboard.application.port.out.FreeBoardEntityQueryResult;
+import tdd.groomingzone.board.freeboard.application.port.out.LoadFreeBoardPort;
+import tdd.groomingzone.board.freeboard.domain.FreeBoard;
+import tdd.groomingzone.comment.freeboardcomment.application.port.in.dto.command.DeleteFreeBoardCommentCommand;
+import tdd.groomingzone.comment.freeboardcomment.application.port.in.dto.command.PostFreeBoardCommentCommand;
+import tdd.groomingzone.comment.freeboardcomment.application.port.in.dto.response.SingleFreeBoardCommentResponse;
+import tdd.groomingzone.comment.freeboardcomment.application.port.out.FreeBoardCommentEntityResult;
+import tdd.groomingzone.comment.freeboardcomment.application.port.out.port.DeleteFreeBoardCommentPort;
+import tdd.groomingzone.comment.freeboardcomment.application.port.out.port.LoadFreeBoardCommentPort;
+import tdd.groomingzone.comment.freeboardcomment.domain.FreeBoardComment;
+import tdd.groomingzone.member.application.port.out.LoadMemberPort;
+import tdd.groomingzone.member.domain.Member;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class DeleteFreeBoardCommentServiceTest {
+    @Mock
+    private LoadMemberPort loadMemberPort;
+
+    @Mock
+    private LoadFreeBoardCommentPort loadFreeBoardCommentPort;
+
+    @Mock
+    private LoadFreeBoardPort loadFreeBoardPort;
+
+    @Mock
+    private FreeBoardCommentPublisher freeBoardCommentPublisher;
+
+    @Mock
+    private DeleteFreeBoardCommentPort deleteFreeBoardCommentPort;
+
+    @InjectMocks
+    private DeleteFreeBoardCommentService deleteFreeBoardCommentService;
+    @Test
+    void testDelete() {
+        //given
+        LocalDateTime testCreatedAt = LocalDateTime.now();
+        LocalDateTime testModifiedAt = LocalDateTime.now();
+        FreeBoardCommentEntityResult freeBoardCommentEntityResult = FreeBoardCommentEntityResult.of(1L,
+                1L,
+                1L,
+                "content",
+                testCreatedAt,
+                testModifiedAt);
+        given(loadFreeBoardCommentPort.loadFreeBoardComment(anyLong())).willReturn(freeBoardCommentEntityResult);
+
+        FreeBoardEntityQueryResult freeBoardEntityQueryResult = FreeBoardEntityQueryResult.of(
+                1L,
+                "title",
+                "content",
+                0,
+                testCreatedAt,
+                testModifiedAt,
+                1L);
+
+        given(loadFreeBoardPort.loadFreeBoardById(anyLong())).willReturn(freeBoardEntityQueryResult);
+
+        Member writer = Member.builder()
+                .memberId(1L)
+                .email("test@email.com")
+                .password("11aA!!@@Password")
+                .phoneNumber("010-1111-1111")
+                .nickName("nickName")
+                .role("BARBER")
+                .build();
+
+        given(loadMemberPort.findMemberById(anyLong())).willReturn(writer);
+
+        FreeBoard freeBoard = FreeBoard.builder()
+                .id(freeBoardEntityQueryResult.getId())
+                .title(freeBoardEntityQueryResult.getTitle())
+                .content(freeBoardEntityQueryResult.getContent())
+                .viewCount(freeBoardEntityQueryResult.getViewCount())
+                .createdAt(freeBoardEntityQueryResult.getCreatedAt())
+                .modifiedAt(freeBoardEntityQueryResult.getModifiedAt())
+                .build();
+        FreeBoardComment freeBoardComment = FreeBoardComment.builder()
+                .id(1L)
+                .freeBoard(freeBoard)
+                .content("content")
+                .writer(writer)
+                .build();
+        given(freeBoardCommentPublisher.createFreeBoardComment(any(), any(), any(), any(), any(), any())).willReturn(freeBoardComment);
+
+        //when
+        DeleteFreeBoardCommentCommand command = DeleteFreeBoardCommentCommand.of(writer.getMemberId(), freeBoard.getId(), freeBoardComment.getId());
+        deleteFreeBoardCommentService.delete(command);
+
+        //then
+        verify(deleteFreeBoardCommentPort).delete(freeBoardComment.getId());
+    }
+}
