69d99d52ae5fc47c423fe5a068cb1823b59d9c28
jkroh1995 <shworud29@gmail.com>
2024-01-02 18:52:22 +0900

    chore : 사용하지 않는 import 제거

--- src/main/java/tdd/groomingzone/auth/application/service/SignOutService.java
+++ src/main/java/tdd/groomingzone/auth/application/service/SignOutService.java
@@ -1,16 +1,13 @@
 package tdd.groomingzone.auth.application.service;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import tdd.groomingzone.auth.application.port.in.usecase.SignOutUseCase;
-import tdd.groomingzone.auth.addapter.out.redis.RedisAdapter;
 import tdd.groomingzone.auth.application.port.out.RedisSignOutPort;
 import tdd.groomingzone.auth.utils.JwtManager;
 import tdd.groomingzone.global.exception.CustomAuthenticationException;
 import tdd.groomingzone.global.exception.ExceptionCode;
 
 @Service
-@Slf4j
 public class SignOutService implements SignOutUseCase {
 
     private final RedisSignOutPort signOutPort;
