75aead241aec4c375d0ddcd58aabba97a3cc0e7a
jkroh1995 <shworud29@gmail.com>
2024-01-25 22:47:58 +0900

    test : 구인구직 게시글 수정 테스트 작성

---
+++ src/test/java/tdd/groomingzone/post/recruitment/adapter/in/web/PutRecruitmentControllerTest.java
@@ -0,0 +1,124 @@
+package tdd.groomingzone.post.recruitment.adapter.in.web;
+
+import com.google.gson.Gson;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.web.servlet.MockMvc;
+import tdd.groomingzone.global.time.Time;
+import tdd.groomingzone.member.domain.Member;
+import tdd.groomingzone.post.recruitment.application.port.in.SingleRecruitmentResponse;
+import tdd.groomingzone.post.recruitment.application.port.in.usecase.PutRecruitmentUseCase;
+import tdd.groomingzone.post.recruitment.domain.Recruitment;
+import tdd.groomingzone.util.MemberCreator;
+import tdd.groomingzone.util.StubTime;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static tdd.groomingzone.global.utils.ApiDocumentUtils.getRequestPreProcessor;
+import static tdd.groomingzone.global.utils.ApiDocumentUtils.getResponsePreProcessor;
+
+@WebMvcTest(controllers = PutRecruitmentController.class,
+        excludeAutoConfiguration = {SecurityAutoConfiguration.class})
+@AutoConfigureRestDocs
+class PutRecruitmentControllerTest {
+
+    @MockBean
+    private SecurityFilterChain oauth;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Gson gson;
+
+    @MockBean
+    private Time stubTime;
+
+    @MockBean
+    private PutRecruitmentUseCase putRecruitmentUseCase;
+
+    @Test
+    @DisplayName("게시글 수정 요청을 보낸다.")
+    void testPutRecruitment() throws Exception {
+        String testTitle = "changedTitle";
+        String testContent = "changedContent";
+        String testType = "OFFER";
+        long testId = 1L;
+
+        RecruitmentApiDto.Put testPutDto = RecruitmentApiDto.Put.builder()
+                .title(testTitle)
+                .content(testContent)
+                .build();
+
+        String content = gson.toJson(testPutDto);
+
+        Member writer = MemberCreator.createMember();
+
+        stubTime = new StubTime(LocalDateTime.of(2023, 11, 28, 22, 30, 10));
+
+        SingleRecruitmentResponse testResponse = SingleRecruitmentResponse.of(Recruitment.builder()
+                .writer(writer)
+                .id(testId)
+                .title(testTitle)
+                .content(testContent)
+                .type(testType)
+                .viewCount(1)
+                .createdAt(LocalDateTime.now())
+                .modifiedAt(stubTime.now())
+                .build());
+
+        given(putRecruitmentUseCase.putRecruitment(any())).willReturn(testResponse);
+
+        mockMvc.perform(
+                        put("/recruitment/{recruitment-id}", testId)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content)
+                ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value(testPutDto.getTitle()))
+                .andExpect(jsonPath("$.content").value(testPutDto.getContent()))
+                .andDo(document("put-recruitment",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        pathParameters(
+                                parameterWithName("recruitment-id").description("구인구직 글 식별자")
+                        ),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+                                        fieldWithPath("content").type(JsonFieldType.STRING).description("내용")
+                                )
+                        ),
+                        responseFields(
+                                fieldWithPath("boardId").type(JsonFieldType.NUMBER).description("게시글 식별자"),
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("내용"),
+                                fieldWithPath("type").type(JsonFieldType.STRING).description("구인구직 머리말"),
+                                fieldWithPath("viewCount").type(JsonFieldType.NUMBER).description("조회수"),
+                                fieldWithPath("createdAt").type(JsonFieldType.STRING).description("작성일"),
+                                fieldWithPath("modifiedAt").type(JsonFieldType.STRING).description("수정일"),
+                                fieldWithPath("writerInfo.writerId").type(JsonFieldType.NUMBER).description("작성한 사용자 ID"),
+                                fieldWithPath("writerInfo.writerNickName").type(JsonFieldType.STRING).description("작성한 사용자 닉네임")
+                        )
+                ));
+    }
+}
---
+++ src/test/java/tdd/groomingzone/post/recruitment/application/service/PutRecruitmentServiceTest.java
@@ -0,0 +1,92 @@
+package tdd.groomingzone.post.recruitment.application.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tdd.groomingzone.member.application.port.out.LoadMemberPort;
+import tdd.groomingzone.member.domain.Member;
+import tdd.groomingzone.post.recruitment.application.port.in.SingleRecruitmentResponse;
+import tdd.groomingzone.post.recruitment.application.port.in.command.PutRecruitmentCommand;
+import tdd.groomingzone.post.recruitment.application.port.out.LoadRecruitmentPort;
+import tdd.groomingzone.post.recruitment.application.port.out.RecruitmentEntityQueryResult;
+import tdd.groomingzone.post.recruitment.application.port.out.SaveRecruitmentPort;
+import tdd.groomingzone.post.recruitment.application.port.out.SaveRecruitmentQuery;
+import tdd.groomingzone.post.recruitment.domain.Recruitment;
+import tdd.groomingzone.util.MemberCreator;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class PutRecruitmentServiceTest {
+
+    @Mock
+    private LoadMemberPort loadMemberPort;
+
+    @Mock
+    private LoadRecruitmentPort loadRecruitmentPort;
+
+    @Mock
+    private SaveRecruitmentPort saveRecruitmentPort;
+
+    @InjectMocks
+    private PutRecruitmentService putRecruitmentService;
+
+    @Test
+    @DisplayName("구인구직 게시글을 수정한다.")
+    void testPutRecruitment() {
+        Member writer = MemberCreator.createMember();
+
+        String testTitle = "title";
+        String testContent = "content";
+        int testViewCount = 1;
+        LocalDateTime testCreatedAt = LocalDateTime.now();
+        LocalDateTime testModifiedAt = LocalDateTime.now();
+        String testType = "OFFER";
+
+        PutRecruitmentCommand putRecruitmentCommand = PutRecruitmentCommand.of(testTitle,
+                testContent,
+                writer.getMemberId(),
+                1L,
+                testModifiedAt);
+
+        SaveRecruitmentQuery saveRecruitmentQuery = SaveRecruitmentQuery.of(Recruitment.builder()
+                .writer(writer)
+                .id(1L)
+                .title(testTitle)
+                .content(testContent)
+                .createdAt(testCreatedAt)
+                .modifiedAt(testModifiedAt)
+                .viewCount(testViewCount)
+                .type(testType)
+                .build());
+
+        RecruitmentEntityQueryResult entityQueryResult = RecruitmentEntityQueryResult.of(1L, writer.getMemberId(), writer.getNickName(), testTitle,testContent, testType, testViewCount, testCreatedAt, testModifiedAt);
+
+
+        given(loadMemberPort.findMemberById(anyLong())).willReturn(writer);
+        given(loadRecruitmentPort.loadRecruitmentById(anyLong())).willReturn(entityQueryResult);
+        given(saveRecruitmentPort.save(any())).willReturn(entityQueryResult);
+
+        SingleRecruitmentResponse response = putRecruitmentService.putRecruitment(putRecruitmentCommand);
+
+        assertThat(response.getBoardId()).isEqualTo(saveRecruitmentQuery.getBoardId());
+        assertThat(response.getTitle()).isEqualTo(saveRecruitmentQuery.getTitle());
+        assertThat(response.getContent()).isEqualTo(saveRecruitmentQuery.getContent());
+        assertThat(response.getViewCount()).isEqualTo(saveRecruitmentQuery.getViewCount());
+        assertThat(response.getCreatedAt()).isEqualTo(saveRecruitmentQuery.getCreatedAt());
+        assertThat(response.getModifiedAt()).isEqualTo(saveRecruitmentQuery.getModifiedAt());
+        assertThat(response.getType()).isEqualTo("구인");
+        assertThat(response.getWriterInfo().getWriterId()).isEqualTo(writer.getMemberId());
+        assertThat(response.getWriterInfo().getWriterNickName()).isEqualTo(writer.getNickName());
+
+    }
+}
